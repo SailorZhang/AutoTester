2679b5f897b5b1ede0d12077b96460dcf05de788
SailorZhang <z@zhangxiangnan.cn>
2016-12-27 18:53:51 +0800

    some update

--- features/step_definitions/documentation.step.js
+++ features/step_definitions/documentation.step.js
@@ -2,23 +2,32 @@ var seleniumWebdriver = require('selenium-webdriver');
 var {until,By} = require('selenium-webdriver');
 var {By:by} = require('protractor').ProtractorBrowser;
 var {defineSupportCode} = require('cucumber');
+var moment = require('moment');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var should = require('chai').should();
+var {expect} = require('chai');
+
+chai.use(chaiAsPromised);
 
 defineSupportCode(function({Given, When, Then}) {
 	Given('I am on the Cucumber.js GitHub repository', function() {
-		var driver = this.driver;
-		var wb = this.wb;
-		driver.ignoreSynchronization = true;
-		driver.get('https://cdm.ciodev.accenture.com');
-		driver.wait(until.titleIs('Sign In'));
-		driver.element(by.id('userNameInput')).sendKeys('ds\\tac.testid001');
-		driver.element(by.id('passwordInput')).sendKeys('hT9a3Vl06gM1kC4yX5cS');
-		return driver.element(by.id('submitButton')).click();
+		return this.login();
 	});
 
 	When('I click on {stringInDoubleQuotes}', function(text) {
-		var EC = this.driver.ExpectedConditions;
-		this.driver.ignoreSynchronization = false;
-		return this.driver.wait(EC.titleIs('Contract Documents Library'));
+		var driver = this.driver;
+		var element = driver.element;
+		var EC = driver.ExpectedConditions;
+
+		driver.wait(EC.visibilityOf(element(by.id('notificationsModal'))));
+		// .then(function() {
+		element(by.id('notificationsModal'))
+			.element(by.tagName('button'))
+			.click();
+
+		driver.wait(EC.invisibilityOf(element(by.id('notificationsModal'))));
+		return element(by.linkText('Favorite Contract Libraries')).click();
 	});
 
 	Then('I should see {stringInDoubleQuotes}', function(text, callback) {
@@ -26,11 +35,35 @@ defineSupportCode(function({Given, When, Then}) {
 		var element = driver.element;
 		var EC = driver.ExpectedConditions;
 
-		driver.wait(EC.visibilityOf(element(by.id('notificationsModal'))))
-			.then(function() {
-				element(by.id('notificationsModal'))
-					.element(by.tagName('button'))
-					.click();
-			});
+		driver.wait(EC.elementToBeClickable(element(by.repeater('record in home.viewData').row(0))));
+		element(by.repeater('record in home.viewData').row(0)).click();
+		var count = 0;
+		element.all(by.repeater('record in filteredRecords')).count().then(function(c) {
+			count = c;
+		});
+
+		driver.$$('.cdmRecords-button').get(1).click();
+		var folderName = "Auto" + moment().format("YYYYmmDDhmmssSSS");
+		element.all(by.model('newFolderName')).get(1).sendKeys(folderName);
+
+		element.all(by.name('newFolderForm')).get(1).element(by.partialButtonText('OK')).click();
+		driver.wait(EC.elementToBeClickable(element(by.repeater('record in filteredRecords').row(0))));
+
+		element.all(by.repeater('record in filteredRecords')).count().then(function(c) {
+			// if (c != count + 2) {
+			expect(c).to.equal(count + 2);
+			// callback("create folder fail!");
+			// }
+		});
+
+		var xpath = "//div[text()='" + folderName + "']";
+
+		driver.findElement(by.xpath(xpath)).then(function(ele) {
+			// driver.actions().mouseMove(ele).doubleClick().perform(); //double click
+			// ele.click(); //click
+			expect(ele.getText()).eventually.equal(folderName).notify(callback);
+		});
+
+		// callback();
 	});
 });
--- features/support/world.js
+++ features/support/world.js
@@ -2,6 +2,8 @@ require('chromedriver');
 var seleniumWebdriver = require('selenium-webdriver');
 var {defineSupportCode} = require('cucumber');
 var protractor = require('protractor');
+var {By:by} = require('protractor').ProtractorBrowser;
+var {until} = require('selenium-webdriver');
 
 function CustomWorld() {
 
@@ -13,8 +15,22 @@ function CustomWorld() {
 
 	this.wb = driver;
 	this.driver = protractor.wrapDriver(driver);
+
 	// this.driver.ignoreSynchronization = true;
 	// this.by = ProtractorBrowser.By;
+	
+	this.login = function(userName='ds\\tac.testid001',password='hT9a3Vl06gM1kC4yX5cS') {
+		var EC = this.driver.ExpectedConditions;
+		this.driver.ignoreSynchronization = true;
+		this.driver.get('https://cdm.ciodev.accenture.com');
+		this.driver.wait(until.titleIs('Sign In'));
+		this.driver.element(by.id('userNameInput')).sendKeys(userName);
+		this.driver.element(by.id('passwordInput')).sendKeys(password);
+		this.driver.element(by.id('submitButton')).click();
+
+		this.driver.ignoreSynchronization = false;
+		return this.driver.wait(EC.titleIs('Contract Documents Library'));
+	}
 }
 
 defineSupportCode(function({setWorldConstructor}) {
